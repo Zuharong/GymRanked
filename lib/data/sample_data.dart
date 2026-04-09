0a4830857021e2d8ebedcd8839720a0b6e8948a8
Fayaaz Abdoel <Fayaaz.Abdoel@student.gildeopleidingen.nl>
2026-04-09 11:30:39 +0200

    add sample exercises for every muscle group

--- lib/data/sample_data.dart
+++ lib/data/sample_data.dart
@@ -6,26 +6,61 @@ final List<MuscleGroup> muscleGroups = [
   MuscleGroup(
     id: 'biceps',
     name: 'Biceps',
-    exerciseIds: ['bicep_curl', 'hammer_curl'],
+    exerciseIds: ['dumbbell_curl', 'barbell_curl'],
+  ),
+  MuscleGroup(
+    id: 'triceps',
+    name: 'Triceps',
+    exerciseIds: ['dumbbell_skull_crusher', 'cable_triceps_extension'],
   ),
   MuscleGroup(
     id: 'chest',
     name: 'Chest',
     exerciseIds: ['bench_press', 'incline_db_press'],
   ),
+  MuscleGroup(
+    id: 'back',
+    name: 'Back',
+    exerciseIds: ['cable_pull_down', 'machine_row'],
+  ),
+  MuscleGroup(
+    id: 'shoulders',
+    name: 'Shoulders',
+    exerciseIds: ['overhead_press', 'lateral_raise'],
+  ),
+  MuscleGroup(
+    id: 'legs',
+    name: 'Legs',
+    exerciseIds: ['squat', 'leg_press'],
+  ),
 ];
 
 final List<Exercise> exercises = [
   Exercise(
-    id: 'bicep_curl',
-    name: 'Bicep Curl',
+    //Biceps
+    id: 'dumbbell_curl',
+    name: 'Dumbbell Curl',
     muscleGroupId: 'biceps',
   ),
   Exercise(
-    id: 'hammer_curl',
-    name: 'Hammer Curl',
+    id: 'barbell_curl',
+    name: 'Barbell Curl',
     muscleGroupId: 'biceps',
   ),
+
+  //triceps
+  Exercise(
+    id: 'dumbbell_skull_crusher',
+    name: 'Dumbbell Skull Crusher',
+    muscleGroupId: 'triceps',
+  ),
+  Exercise(
+    id: 'cable_triceps_extension',
+    name: 'Cable Triceps Extension',
+    muscleGroupId: 'triceps',
+  ),
+
+  //chest
   Exercise(
     id: 'bench_press',
     name: 'Bench Press',
@@ -36,8 +71,46 @@ final List<Exercise> exercises = [
     name: 'Incline Dumbbell Press',
     muscleGroupId: 'chest',
   ),
+
+  //back
+  Exercise(
+    id: 'cable_pull_down',
+    name: 'Cable Pull-Down',
+    muscleGroupId: 'back',
+  ),
+  Exercise(
+    id: 'machine_row',
+    name: 'Machine Row',
+    muscleGroupId: 'back',
+  ),
+
+  //shoulders
+  Exercise(
+    id: 'overhead_press',
+    name: 'Overhead Press',
+    muscleGroupId: 'shoulders',
+  ),
+  Exercise(
+    id: 'lateral_raise',
+    name: 'Lateral Raise',
+    muscleGroupId: 'shoulders',
+  ),
+
+  //legs
+  Exercise(
+    id: 'squat',
+    name: 'Squat',
+    muscleGroupId: 'legs',
+  ),
+  Exercise(
+    id: 'leg_press',
+    name: 'Leg Press',
+    muscleGroupId: 'legs',
+  ),
+
 ];
 
+//inserts 
 final List<WorkoutEntry> workoutEntries = [
   WorkoutEntry(
     id: '1',
